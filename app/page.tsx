c82da2f76f1e94c71de740d6b115e76763534fd7
Rosaura <rosaura.quintana@anahuac.mx>
2024-11-19 13:40:09 -0600

    Puse un lol q mal que deberia estar en rojo pero no esta sirviendo

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-
+import styles from './styles/Home.module.css'
 
 export default async function Index() {
   return (
     <>
       <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Rosaura trabaja aquí</h2>
-
+        <h2 className={styles.estiloprueba}>Rosaura trabaja aquí</h2>
+        <p className={styles.estiloprueba}>lol q mal</p>
+        
       </main>
     </>
   );
