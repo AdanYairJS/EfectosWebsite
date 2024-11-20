356493d11eb0cef00ba37b38f3bf5fbd0d1de895
AdanYairJS <adan_jimenez@anahuac.mx>
2024-11-20 00:23:51 -0600

    Creado componente product card y avances en la pestaña de productos

---
+++ app/constants/products.ts
@@ -0,0 +1,39 @@
+export const products = [
+    {
+      id: 1,
+      name: "Bolsa Negra",
+      image: "/productos/producto1.png",
+      price: "$1,200",
+    },
+    {
+      id: 2,
+      name: "Bolsa Roja",
+      image: "/productos/producto2.png",
+      price: "$1,500",
+    },
+    {
+      id: 3,
+      name: "Bolsa Azul",
+      image: "/productos/producto3.png",
+      price: "$1,800",
+    },
+    {
+      id: 4,
+      name: "Bolsa Verde",
+      image: "/productos/producto4.png",
+      price: "$1,300",
+    },
+    {
+      id: 5,
+      name: "Bolsa Amarilla",
+      image: "/productos/producto5.png",
+      price: "$1,100",
+    },
+    {
+        id: 6,
+        name: "Bolsa Blanca",
+        image: "/productos/producto6.png",
+        price: "$1,150",
+      },
+  ];
+  
--- app/productos/page.tsx
+++ app/productos/page.tsx
@@ -1,5 +1,7 @@
 import { createClient } from '@/utils/supabase/server';
 import styles from '../styles/Productos.module.css'
+import { products } from '../constants/products';
+import Card from '../components/ProductCard';
 
 export default async function Productos() {
   const supabase = await createClient(); // Espera a que la promesa se resuelva
@@ -13,7 +15,16 @@ export default async function Productos() {
         <p className={styles.text}>Descubre nuestra colección de bolsas únicas para cada ocasión. Con nuestro sistema de apartado, puedes reservar tus favoritas con facilidad.</p>
         <div></div>
       </section>
-
+      <div className={styles.cardsContainer}>
+        {products.map((product) => (
+          <Card
+            key={product.id}
+            image={product.image}
+            name={product.name}
+            price={product.price}
+          />
+        ))}
+      </div>
     </main>
   );
 }
