c7e5baf3f25326e31f017b9752e8756404f296ea
AdanYairJS <adan_jimenez@anahuac.mx>
2024-11-26 14:41:51 -0600

    Primera version de la pagina de apartados, adicion de estilos a la pagina de productos

--- app/apartados/page.tsx
+++ app/apartados/page.tsx
@@ -1,11 +1,46 @@
-import styles from '../styles/Productos.module.css'
+import styles from "../styles/Apartados.module.css";
 
 export default async function Index() {
-    return (
-      <>
-        <main className="flex-1 flex flex-col gap-6 px-4">
-          <h2 className={styles.textofancy}>Apartados</h2>
-        </main>
-      </>
-    );
-  }
+  const steps = [
+    "Selecciona el producto que deseas apartar.",
+    "Haz clic en el botón 'Apartar'.",
+    "Llena el formulario con tus datos.",
+    "Confirma tu apartado y espera la notificación.",
+  ];
+
+  return (
+    <>
+      <main className="flex-1 flex flex-col gap-6 px-4">
+        <section className={styles.section}>
+          <div className={styles.imageTextContainer}>
+            <img
+              src="apartados/apartados.png"
+              alt="Apartados"
+              className={styles.image}
+            />
+            <div className={styles.textContainer}>
+              <h2 className={styles.title}>Apartados</h2>
+              <p className={styles.paragraph}>
+                En nuestra tienda, puedes apartar tus productos favoritos con
+                facilidad. Sigue los pasos indicados y asegura tus artículos
+                preferidos antes de que se agoten.
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <section className={styles.stepsSection}>
+          <h3 className={styles.stepsTitle}>Pasos para apartar un producto</h3>
+          <ul className={styles.stepsList}>
+            {steps.map((step, index) => (
+              <li key={index} className={styles.stepItem}>
+                <div className={styles.stepNumber}>{index + 1}</div>
+                <p className={styles.stepText}>{step}</p>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </main>
+    </>
+  );
+}
--- app/productos/page.tsx
+++ app/productos/page.tsx
@@ -12,9 +12,7 @@ export default async function Productos() {
     <main>
       <section className={styles.section_header}>
         <h2 className={styles.title}>Bolsas</h2>
-        <p className={styles.text}>Descubre nuestra colección de bolsas únicas para cada ocasión. Con nuestro sistema de apartado, puedes reservar tus favoritas con facilidad.</p>
-        <div></div>
-      </section>
+        <p className={styles.text}>Descubre nuestra colección de bolsas únicas para cada ocasión. Con nuestro sistema de apartado, puedes reservar tus favoritas con facilidad.</p>      </section>
       <div className={styles.cardsContainer}>
         {products.map((product) => (
           <Card
