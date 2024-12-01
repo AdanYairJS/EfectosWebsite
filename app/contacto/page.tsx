674e334e6e12beb291a71e0e1bf31d4d857e5e12
Rosaura <rosaura.quintana@anahuac.mx>
2024-11-30 23:18:02 -0600

    hoy hicimos todoooo

--- app/contacto/page.tsx
+++ app/contacto/page.tsx
@@ -8,21 +8,34 @@ const Contacto = () => {
         <div className={styles.contactInfo}>
           <h1 className={styles.contactTitle}>Contacto</h1>
           <p>
-            ¿Tienes alguna pregunta o necesitas ayuda para elegir el accesorio perfecto? En Efectos estamos aquí para ayudarte. Completa el formulario a continuación o envíanos un correo directamente. Nos encanta escuchar a nuestros clientes y resolver cualquier duda que tengan.
+            ¡Estamos aquí para ti! Visítanos en nuestra tienda física o escríbenos para cualquier duda o consulta. 
           </p>
           <p>
-            Para consulta directa, contáctanos a: <strong>contacto@efectos.com</strong>
+            ¿Buscas el accesorio perfecto? ¡Déjanos ayudarte a encontrarlo! 
+          </p>
+          <p>
+            Horarios de atención: Lunes a Viernes de 11:00 AM a 2:00 PM y de 3:00 PM a 9:00 PM.
+          </p>
+          <p>
+            <strong>Dirección:</strong> Al interior del centro comercial Macroplaza Oaxaca local 8 planta baja.
+          </p>
+          <p>
+            <strong>Teléfono:</strong> (+52) 1 951 408 6830
+          </p>
+          <p>
+            <strong>Correo Electrónico:</strong> contacto@efectos.com
           </p>
         </div>
-
-        {/* Formulario de contacto */}
-        <form className={styles.contactForm}>
-          <input type="text" placeholder="NOMBRE" className={styles.inputField} />
-          <input type="email" placeholder="E-MAIL" className={styles.inputField} />
-          <input type="text" placeholder="ASUNTO" className={styles.inputField} />
-          <textarea placeholder="MENSAJE" className={styles.textArea}></textarea>
-          <button type="submit" className={styles.submitButton}>ENVIAR</button>
-        </form>
+        <div className={styles.mapContainer}>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3625.3766922170903!2d-96.69709312507291!3d17.068008583765685!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85c723c5c2a05369%3A0x6bada18c9f801410!2sMacroplaza!5e1!3m2!1ses-419!2smx!4v1733025119269!5m2!1ses-419!2smx"
+          width="100%" 
+          height="100%" 
+          allowFullScreen={true} 
+          loading="lazy" 
+          title="Mapa del restaurante"
+        ></iframe>
+      </div>
       </section>
     </main>
   );
